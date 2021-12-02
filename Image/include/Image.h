e4347f647c20374aaebd646f288f23877b2e428f
Aslan Saparov <i7654008@w11912.student.bournemouth.ac.uk>
2021-12-02 14:06:02 +0000

    added Particle

--- Image/include/Image.h
+++ Image/include/Image.h
@@ -1,7 +1,10 @@
 #ifndef TEST_H_
 #define TEST_H_
+
 #include <cstdint>
 #include <cstdlib>
+#include <memory>
+#include <string>
 
 struct RGBA
 {
@@ -24,12 +27,20 @@ struct RGBA
 class Image
 {
     public:
+    Image()=default;
+    Image(size_t _w, size_t _h);
+    Image(size_t _w, size_t _h, unsigned char _r, unsigned char _g, unsigned char _b, unsigned char _a);
+    bool write(const std::string &_fname) const;
     size_t width() const;
     size_t height() const;
+    RGBA getPixel(size_t _x, size_t _y);
+    void setPixel(size_t _x, size_t _y, RGBA _p);
+    void line(int _sx, int _sy, int _ex, int _ey, unsigned char _r, unsigned char _g, unsigned char _b, unsigned char _a);
 
     private:
     size_t m_width=0;
     size_t m_height=0;
+    std::unique_ptr<RGBA[]> m_pixels;
 };
 
 #endif
